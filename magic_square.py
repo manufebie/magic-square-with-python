bb440b6a1a975b7c77d72b8996f7d451b482ed07
manufebie <manu.febie@gmail.com>
2018-09-26 23:55:38 +0700

    Code is stil not finished. Could not think of a way to fill the square the way a magic square should be filled.

---
+++ magic_square.py
@@ -0,0 +1,20 @@
+'''
+Example magic square if n=3
+   OUTPUT
+[2] [7] [6]
+[9] [5] [1] <- Start position
+[4] [3] [8]
+'''
+# generate the matrix based on n input
+n = int(input('Enter an "odd" number: '))
+square = [[0 for x in range(n)] for y in range(n)]
+magic_number = n**2 # constant 
+
+# Set start position for 1
+# loop while < n**2 
+# fill square
+
+
+
+
+
